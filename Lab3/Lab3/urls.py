bb8afdcc5474407f505071fc04fd6ab64027c24d
radwa nabil <radwaismail444@gmail.com>
2023-06-18 13:47:34 +0300

    lab3 and lab4

--- Lab3/Lab3/urls.py
+++ Lab3/Lab3/urls.py
@@ -1,5 +1,5 @@
 """
-URL configuration for bookstoreLab1 project.
+URL configuration for Lab3 project.
 
 The `urlpatterns` list routes URLs to views. For more information please see:
     https://docs.djangoproject.com/en/4.2/topics/http/urls/
@@ -15,14 +15,11 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
-from django.conf import settings
+from django.urls import path,include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('bookstore/', include('bookstore.urls')),
+    path('', include('Home.urls')),
+    path("user/" , include('user.urls') , name="user")
 ]
 
-if settings.DEBUG:
-    from django.conf.urls.static import static
-    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
